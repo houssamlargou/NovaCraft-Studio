5a0354df6d02e70ed9ba5ac33e63e6bbc9e2e540
houssam largou <hossamlargo5@gmail.com>
2025-12-04 07:39:14 +0100

    feat: implement homepage hero section

--- views/home.php
+++ views/home.php
@@ -1,2 +1,18 @@
-<?php
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style/output.css">
+</head>
+<body>
+    <?php require(__DIR__ . "/../templates/header.php"); ?> 
+    <section class="py-16 px-24 flex flex-col gap-7 items-center">
+        <div class="text-3xl font-semibold" >Transformez votre présence digitale</div>
+        <p class="text-2xl text-neutral-500">Sites web modernes, design UI/UX, et solutions digitales sur mesure pour votre entreprise.</p>
+        <button class="bg-blue-700 text-neutral-50 font-semibold py-3 px-4 rounded-lg hover:cursor-pointer">Découvrir nos services</button>
+    </section>
+    <?php require(__DIR__ . "/../templates/footer.php") ?>
+</body>
+</html>
