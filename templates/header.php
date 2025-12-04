89aada92bb26b14dfd383d7f25aa2fb56026f27d
houssam largou <hossamlargo5@gmail.com>
2025-12-04 05:23:48 +0100

    feat: add header template

--- templates/header.php
+++ templates/header.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/style/output.css">
+    <link rel="stylesheet" href="../style/output.css">
 </head>
 <body>
-
+    <section class="flex justify-between p-4 bg-gray-300">
+        <div class="bolder text-3xl font-bold">NovaCraft</div>
+        <ul class="flex items-center gap-4">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
 </body>
 </html>
